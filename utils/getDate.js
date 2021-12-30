bc9afddd776d85cce8e8b85990a6991a4bb91eec
adityaharsh2001 <geekyadi118@gmail.com>
2021-12-30 19:21:23 +0530

    contest reminder

---
+++ utils/getDate.js
@@ -0,0 +1,51 @@
+const getDate = (keyWords) => {
+  if(keyWords=="live") {
+    var status = "Live"
+var currentdate = new Date(); 
+var start = currentdate.getFullYear() + "-" 
+    + (currentdate.getMonth()+1) + "-" 
+    + currentdate.getDate() + "T"
+    + currentdate.getHours() + ":"  
+    + currentdate.getMinutes() + ":" 
+    + currentdate.getSeconds();
+    
+    var date= `start__lte=${start}%26end__gte=${start}`
+
+}
+if(keyWords=="future") {
+    var status = "Furure"
+    var currentdate = new Date(); 
+    var start = (currentdate.getFullYear()) + "-" 
+        + (currentdate.getMonth()+1) + "-" 
+        + currentdate.getDate() + "T"
+        + currentdate.getHours() + ":"  
+        + currentdate.getMinutes() + ":" 
+        + currentdate.getSeconds();
+        
+        var date= `start__gte=${start}`
+    
+    }
+    if(keyWords=="past") {
+        var status = "Past"
+        var currentdate = new Date(); 
+        var end = currentdate.getFullYear() + "-" 
+            + (currentdate.getMonth()+1) + "-" 
+            + currentdate.getDate() + "T"
+            + currentdate.getHours() + ":"  
+            + currentdate.getMinutes() + ":" 
+            + currentdate.getSeconds();
+       
+        var start = currentdate.getFullYear() + "-" 
+            + (currentdate.getMonth()) + "-" 
+            + currentdate.getDate() + "T"
+            + currentdate.getHours() + ":"  
+            + currentdate.getMinutes() + ":" 
+            + currentdate.getSeconds();
+        var date= `start__gte=${start}%26end__lte=${end}`
+        }
+        return { 
+          date, 
+          status
+        }
+}
+module.exports = getDate
